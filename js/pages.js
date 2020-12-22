d168c42cd9f0a7bb33607b3e1a3d76f33c72374a
Dmytro Borodin <dimabor96@gmail.com>
2020-12-22 11:19:28 +0200

    height1

--- js/pages.js
+++ js/pages.js
@@ -129,3 +129,12 @@ showFullTitle();
 window.addEventListener('resize', showFullTitle)
 
 
+//Mobile view
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
