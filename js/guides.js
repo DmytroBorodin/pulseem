f06c2ff7db140939314a0ff43e3f622e1699e021
Dmytro Borodin <dimabor96@gmail.com>
2020-12-22 12:20:18 +0200

    guides

--- js/guides.js
+++ js/guides.js
@@ -57,3 +57,10 @@ selectBlock.addEventListener('change', (e) => {
 	}
 })
 
+//Mobile view
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
--- js/main.js
+++ js/main.js
@@ -180,3 +180,11 @@ shortCardsArr.forEach(card => {
 }) 
 
 
+//Mobile view
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
